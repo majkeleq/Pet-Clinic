84f794dee1c3e24dfa6683dfa4e42a7942679bd4
majkeleq <graf3rpl@gmail.com>
2024-03-12 14:16:41 +0100

    feat(pet): added getPet(id)

--- src/main/java/org/example/petclinictest/businesslayer/pet/PetController.java
+++ src/main/java/org/example/petclinictest/businesslayer/pet/PetController.java
@@ -1,8 +1,10 @@
 package org.example.petclinictest.businesslayer.pet;
 
+import org.example.petclinictest.businesslayer.exceptions.PetNotFoundException;
 import org.example.petclinictest.businesslayer.mappers.PetMapper;
 import org.example.petclinictest.persistancelayer.PetRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,12 @@ public class PetController {
         this.petMapper = petMapper;
     }
 
-    @GetMapping()
+    @GetMapping("/{id}")
+
+    public PetDTO getPet(@PathVariable Long id) {
+        return petMapper.mapToPetDTO(petRepository.findById(id).orElseThrow(() -> new PetNotFoundException(id)));
+    }
+    @GetMapping
     public List<PetDTO> getAllPets() {
         return petRepository.findAll().stream().map(petMapper::mapToPetDTO).toList();
     }
