ff8490ab433a97524ca30be302dbeea8e7b8872f
majkeleq <graf3rpl@gmail.com>
2024-03-13 12:53:56 +0100

    feat(db): corrected table's primary keys, added foreign key to owner for pet

--- src/main/java/org/example/petclinictest/businesslayer/owner/Owner.java
+++ src/main/java/org/example/petclinictest/businesslayer/owner/Owner.java
@@ -1,9 +1,17 @@
 package org.example.petclinictest.businesslayer.owner;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import org.example.petclinictest.businesslayer.Person;
+import org.example.petclinictest.businesslayer.pet.Pet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Owner extends Person {
 
+    @OneToMany(mappedBy = "owner", cascade = CascadeType.REMOVE, orphanRemoval=true)
+    private List<Pet> pets = new ArrayList<>();
 }
