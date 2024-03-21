d9af4763762d5073d943a5c893c88e285c7c4470
majkeleq <graf3rpl@gmail.com>
2024-03-21 12:41:49 +0100

    feat(api): Ignoring Pets list while mapping OwnerDTO to Owner

--- src/main/java/org/example/petclinictest/businesslayer/mappers/OwnerMapper.java
+++ src/main/java/org/example/petclinictest/businesslayer/mappers/OwnerMapper.java
@@ -3,11 +3,14 @@ package org.example.petclinictest.businesslayer.mappers;
 import org.example.petclinictest.businesslayer.owner.Owner;
 import org.example.petclinictest.businesslayer.owner.OwnerDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface OwnerMapper {
 
+    @Mapping(ignore = true, target = "pets")
     Owner mapToOwner(OwnerDTO dto);
+
 
     OwnerDTO mapToOwnerDTO(Owner owner);
 }
